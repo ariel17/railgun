f0ee7bb2d301fe3f5167f27f59625001ddaaad05
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2020-11-03 01:40:51 -0300

    Changes dependency injection strategy.

--- api/database/database.go
+++ api/database/database.go
@@ -1,25 +1,18 @@
 package database
 
-import (
-	"github.com/ariel17/railgun/api/config"
-)
-
 type DB interface {
 	Select(dest interface{}, query string, args ...interface{}) error
 }
 
 var (
-	isProduction func() bool
+	implementation func() DB
 )
 
 // New creates an instance of the database driver based on the environment.
 func New() DB {
-	if isProduction() {
-		return newMySQL()
-	}
-	return newMock()
+	return implementation()
 }
 
 func init() {
-	isProduction = config.IsProduction
+	implementation = newMySQL
 }
