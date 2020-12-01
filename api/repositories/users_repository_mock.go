165b22c4757714ea5660035bd70b6ea833cfa494
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2020-12-01 00:07:46 -0300

    Adds more test units on factories.

--- api/repositories/users_repository_mock.go
+++ api/repositories/users_repository_mock.go
@@ -2,19 +2,19 @@ package repositories
 
 import "github.com/ariel17/railgun/api/entities"
 
-type MockRepository struct {
+type MockUsersRepository struct {
 	User *entities.User
 	Err  error
 }
 
-func (m *MockRepository) GetByID(_ string) (*entities.User, error) {
+func (m *MockUsersRepository) GetByID(_ string) (*entities.User, error) {
 	return m.User, m.Err
 }
 
-func (m *MockRepository) DeleteByID(_ string) error {
+func (m *MockUsersRepository) DeleteByID(_ string) error {
 	return m.Err
 }
 
 func newUsersRepositoryMock() UsersRepository {
-	return &MockRepository{}
+	return &MockUsersRepository{}
 }
--- api/repositories/users_repository_test.go
+++ api/repositories/users_repository_test.go
@@ -1,63 +1,14 @@
 package repositories
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/ariel17/railgun/api/entities"
 )
 
-func TestUsersRepository(t *testing.T) {
+func TestNewUsersRepository(t *testing.T) {
 	usersRepositoryImplementation = newUsersRepositoryMock
-	id := "auth0-1234-fake"
-
-	t.Run("get by id", func(t *testing.T) {
-		testCases := []struct {
-			name       string
-			exists     bool
-			successful bool
-		}{
-			{"exists and successful", true, true},
-			{"does not exist and successful", false, true},
-			{"could exist but fails", true, false},
-		}
-		for _, tc := range testCases {
-			t.Run(tc.name, func(t *testing.T) {
-				repository := NewUsersRepository()
-				if tc.successful {
-					if tc.exists {
-						repository.(*MockRepository).User = &entities.User{ID: id}
-					}
-				} else {
-					repository.(*MockRepository).Err = errors.New("mocked error")
-				}
-				user, err := repository.GetByID(id)
-				assert.Equal(t, err == nil, tc.successful)
-				assert.Equal(t, user != nil, tc.exists && tc.successful)
-			})
-		}
-	})
-
-	t.Run("delete by id", func(t *testing.T) {
-		id := "auth0-1234-fake"
-		testCases := []struct {
-			name       string
-			successful bool
-		}{
-			{"successful", true},
-			{"fails", false},
-		}
-		for _, tc := range testCases {
-			t.Run(tc.name, func(t *testing.T) {
-				repository := NewUsersRepository()
-				if !tc.successful {
-					repository.(*MockRepository).Err = errors.New("mocked error")
-				}
-				err := repository.DeleteByID(id)
-				assert.Equal(t, err == nil, tc.successful)
-			})
-		}
-	})
+	r := NewUsersRepository()
+	_, ok := r.(*MockUsersRepository)
+	assert.True(t, ok)
 }
