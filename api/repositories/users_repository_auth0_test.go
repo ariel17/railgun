753cd79b74e4f60103beae464d9ba6648776c835
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2020-12-05 21:17:08 -0300

    Adds more test units for Auth0.

--- api/repositories/users_repository_auth0_test.go
+++ api/repositories/users_repository_auth0_test.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,22 +11,44 @@ import (
 )
 
 func TestAuth0Repository_GetByID(t *testing.T) {
-	id := "still-fake"
-	r := newUsersRepositoryAuth0().(*auth0Repository)
-	r.m = &auth0.MockUserManager{
-		User: &management.User{
-			ID: &id,
-		},
-		Err: nil,
-	}
-	u, err := r.GetByID("fake-id")
-	assert.Nil(t, err)
-	assert.Equal(t, id, u.ID)
+	t.Run("ok", func(t *testing.T) {
+		id := "still-fake"
+		r := newUsersRepositoryAuth0().(*auth0Repository)
+		r.m = &auth0.MockUserManager{
+			User: &management.User{
+				ID: &id,
+			},
+			Err: nil,
+		}
+		u, err := r.GetByID("fake-id")
+		assert.Nil(t, err)
+		assert.Equal(t, id, u.ID)
+	})
+
+	t.Run("failed", func(t *testing.T) {
+		r := newUsersRepositoryAuth0().(*auth0Repository)
+		r.m = &auth0.MockUserManager{
+			Err: errors.New("mocked error"),
+		}
+		_, err := r.GetByID("fake-id")
+		assert.NotNil(t, err)
+	})
 }
 
 func TestAuth0Repository_DeleteByID(t *testing.T) {
-	r := newUsersRepositoryAuth0().(*auth0Repository)
-	r.m = &auth0.MockUserManager{}
-	err := r.DeleteByID("fake-id")
-	assert.Nil(t, err)
+	t.Run("ok", func(t *testing.T) {
+		r := newUsersRepositoryAuth0().(*auth0Repository)
+		r.m = &auth0.MockUserManager{}
+		err := r.DeleteByID("fake-id")
+		assert.Nil(t, err)
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		r := newUsersRepositoryAuth0().(*auth0Repository)
+		r.m = &auth0.MockUserManager{
+			Err: errors.New("mocked error"),
+		}
+		err := r.DeleteByID("fake-id")
+		assert.NotNil(t, err)
+	})
 }
