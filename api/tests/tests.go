9960c249c1653f5a5ab0745dc358abf3a4f996ee
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2020-12-11 19:12:17 -0300

    Adds source for function.

--- api/tests/tests.go
+++ api/tests/tests.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+// GetGoldenFile reads the content of the file in the given path.
+// Source: https://medium.com/@jarifibrahim/golden-files-why-you-should-use-them-47087ec994bf
 func GetGoldenFile(t *testing.T, path string) []byte {
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
